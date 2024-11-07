f42658ddf4a1f0477e0124953f766f8399845918
Jasper Evens <r0895652@ucll.be>
2024-11-07 14:33:52 +0100

    typo

--- front-end/components/patients/PatientOverviewTable.tsx
+++ front-end/components/patients/PatientOverviewTable.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Patient } from '@types';
+import React from 'react'; 
+import { Patient } from 'types';
 
 interface PatientOverviewTableProps {
     patients: Array<Patient>;
